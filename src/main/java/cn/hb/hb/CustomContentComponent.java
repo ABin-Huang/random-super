0af732b5f37e9850230be6d50797bee255a449d8
hb <2423244747@qq.com>
2024-09-17 22:10:17 +0800

    feat：完成字符串和数字生成功能，完善交互界面，修改插件简介

--- src/main/java/cn/hb/hb/CustomContentComponent.java
+++ src/main/java/cn/hb/hb/CustomContentComponent.java
@@ -1,14 +1,31 @@
 package cn.hb.hb;
 
 
+import cn.hb.core.BaseFuncParam;
+import cn.hb.core.BaseFunction;
+import cn.hb.enums.TypeEnum;
+import cn.hb.factory.FuncFactory;
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.command.WriteCommandAction;
+import com.intellij.openapi.editor.CaretModel;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.project.Project;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.util.Objects;
 
-public class CustomContentComponent extends JPanel {
+/**
+ * The type Custom content component.
+ */
+public class CustomContentComponent extends JPanel implements Disposable {
     private JTextField textField;
     private JButton button;
     private JRadioButton radioButton1;
@@ -17,47 +34,31 @@ public class CustomContentComponent extends JPanel {
     private ButtonGroup buttonGroup;
     private String selectedValue = "";
 
-    public CustomContentComponent() {
+    /**
+     * Instantiates a new Custom content component.
+     *
+     * @param actionEvent the action event
+     */
+    public CustomContentComponent(AnActionEvent actionEvent) {
         setLayout(new BorderLayout());
 
         // 创建一个文本框
-        textField = new JTextField("Enter your name here", 20);
-        add(textField, BorderLayout.NORTH);
+//        textField = new JTextField("Enter your name here", 20);
+//        add(textField, BorderLayout.NORTH);
 
         // 创建单选按钮
         buttonGroup = new ButtonGroup();
-        radioButton1 = new JRadioButton("int");
-        radioButton2 = new JRadioButton("string");
-        radioButton3 = new JRadioButton("id");
+        radioButton1 = new JRadioButton(TypeEnum.INTEGER.getName());
+        radioButton2 = new JRadioButton(TypeEnum.STRING.getName());
+        radioButton3 = new JRadioButton(TypeEnum.ID.getName());
 
         buttonGroup.add(radioButton1);
         buttonGroup.add(radioButton2);
         buttonGroup.add(radioButton3);
 
-        // 监听单选按钮的变化
-        radioButton1.addItemListener(e -> {
-            if (e.getStateChange() == ItemEvent.SELECTED) {
-                selectedValue = radioButton1.getText();
-            }
-        });
-
-        radioButton2.addItemListener(new ItemListener() {
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-                if (e.getStateChange() == ItemEvent.SELECTED) {
-                    selectedValue = radioButton2.getText();
-                }
-            }
-        });
-
-        radioButton3.addItemListener(new ItemListener() {
-            @Override
-            public void itemStateChanged(ItemEvent e) {
-                if (e.getStateChange() == ItemEvent.SELECTED) {
-                    selectedValue = radioButton3.getText();
-                }
-            }
-        });
+        addButtonEvent(radioButton1, actionEvent);
+        addButtonEvent(radioButton2, actionEvent);
+        addButtonEvent(radioButton3, actionEvent);
 
         JPanel radioPanel = new JPanel();
         radioPanel.setLayout(new BoxLayout(radioPanel, BoxLayout.X_AXIS));
@@ -67,32 +68,111 @@ public class CustomContentComponent extends JPanel {
 
         add(radioPanel, BorderLayout.CENTER);
 
-        // 创建一个按钮
-        button = new JButton("Submit");
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String name = textField.getText();
-                System.out.println("我选择的是：" +selectedValue);
-                JOptionPane.showMessageDialog(CustomContentComponent.this, "Hello, " + name + "! Selected Option: " + selectedValue);
-            }
-        });
-        add(button, BorderLayout.SOUTH);
     }
 
+    /**
+     * 监听按钮事件.
+     *
+     * @param button      the button
+     * @param actionEvent the action event
+     */
+    public void addButtonEvent(JRadioButton button, AnActionEvent actionEvent) {
+        // 监听单选按钮的变化
+        button.addItemListener(e -> {
+
+            if (e.getStateChange() == ItemEvent.SELECTED) {
+
+                selectedValue = button.getText();
+                Editor editor = actionEvent.getData(CommonDataKeys.EDITOR);
+                Project project = actionEvent.getProject();
+                System.out.println("我选择的：" +selectedValue);
+                String res = null;
+                try {
+                    BaseFunction baseFunction = FuncFactory.get(TypeEnum.ofType(selectedValue));
+                    if (Objects.isNull(baseFunction)) {
+
+                        throw new Exception("该类型未实现");
+                    }
+                    res = baseFunction.getResult(new BaseFuncParam());
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+                insertTextAtMousePosition(editor, project, res);
+
+                JComponent comp = (JComponent) e.getSource();
+                Window win = SwingUtilities.getWindowAncestor(comp);
+                win.dispose();
+            }
+        });
+    }
+
+    /**
+     * Close panel.
+     *
+     * @param e the e
+     */
+    public static void closePanel(ActionEvent e) {
+        JComponent comp = (JComponent) e.getSource();
+        Window win = SwingUtilities.getWindowAncestor(comp);
+        win.dispose();
+    }
+
+    /**
+     * Insert text at mouse position.
+     *
+     * @param editor  the editor
+     * @param project the project
+     */
+    public static void insertTextAtMousePosition(Editor editor, Project project, String result) {
+        WriteCommandAction.runWriteCommandAction(project, () -> {
+            Document document = editor.getDocument();
+            int offset = editor.getCaretModel().getCurrentCaret().getOffset();
+            document.insertString(offset, result);
+            // 将光标移动到插入文本的结束位置
+            CaretModel caretModel = editor.getCaretModel();
+            caretModel.moveToOffset(offset + result.length());
+        });
+    }
+
+    /**
+     * Gets text field.
+     *
+     * @return the text field
+     */
     public JTextField getTextField() {
         return textField;
     }
 
+    /**
+     * Gets radio button 1.
+     *
+     * @return the radio button 1
+     */
     public JRadioButton getRadioButton1() {
         return radioButton1;
     }
 
+    /**
+     * Gets radio button 2.
+     *
+     * @return the radio button 2
+     */
     public JRadioButton getRadioButton2() {
         return radioButton2;
     }
 
+    /**
+     * Gets radio button 3.
+     *
+     * @return the radio button 3
+     */
     public JRadioButton getRadioButton3() {
         return radioButton3;
+    }
+
+
+    @Override
+    public void dispose() {
+
     }
 }
--- src/main/java/cn/hb/hb/DDDDDDAction.java
+++ src/main/java/cn/hb/hb/DDDDDDAction.java
@@ -2,8 +2,6 @@ package cn.hb.hb;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.ui.MessageType;
-import com.intellij.openapi.ui.popup.Balloon;
 import com.intellij.openapi.ui.popup.ComponentPopupBuilder;
 import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.openapi.ui.popup.JBPopupFactory;
@@ -11,8 +9,6 @@ import com.intellij.openapi.wm.StatusBar;
 import com.intellij.openapi.wm.WindowManager;
 import com.intellij.ui.awt.RelativePoint;
 
-import javax.swing.*;
-import javax.swing.event.HyperlinkListener;
 import java.awt.*;
 
 public class DDDDDDAction extends AnAction {
@@ -25,22 +21,8 @@ public class DDDDDDAction extends AnAction {
         // 创建JBPopupFactory实例
         JBPopupFactory popupFactory = JBPopupFactory.getInstance();
 
-        // 创建一个带有自定义内容的弹窗
-//        Balloon balloon = popupFactory.createHtmlTextBalloonBuilder(
-//                "<html><body><p style='color:#00FF00;'>Action performed<br>操作已执行</p></body></html>", // HTML格式文本内容
-//                null, // 不提供图标
-//                statusBar.getComponent()[0], // 在状态栏的第一个组件的位置显示
-//                Balloon.Position.atRight, // 以状态栏第一个组件的右侧显示
-//                null // 可以添加一个Listener来监听关闭事件
-//        ).createBalloon();
-
-        // popupFactory.createConfirmation("hb_tab", null, 1).showInFocusCenter();
-
         // 创建自定义的内容组件
-        CustomContentComponent customContent = new CustomContentComponent();
-
-        // 创建自定义的 JComponent (例如一个文本框)
-        CustomTextField textField = new CustomTextField("Enter some text");
+        CustomContentComponent customContent = new CustomContentComponent(e);
 
         // 使用 JBPopupFactory 创建弹窗构建器，并指定内容组件和首选焦点组件
         ComponentPopupBuilder popupBuilder = JBPopupFactory.getInstance()
@@ -74,34 +56,5 @@ public class DDDDDDAction extends AnAction {
         // 显示弹窗
         popup.show(centerPoint);
 
-    }
-
-    public class CustomComponent extends JComponent {
-
-        private String message = "Hello, Custom Component!";
-
-        @Override
-        protected void paintComponent(Graphics g) {
-            super.paintComponent(g);
-
-            // 绘制背景颜色
-            g.setColor(Color.lightGray);
-            g.fillRect(0, 0, getWidth(), getHeight());
-
-            // 绘制矩形
-            g.setColor(Color.BLUE);
-            g.drawRect(10, 10, 100, 50);
-
-            // 绘制文本
-            g.setColor(Color.BLACK);
-            Font font = new Font("Serif", Font.BOLD, 18);
-            g.setFont(font);
-            g.drawString(message, 15, 70);
-        }
-
-        public void setMessage(String message) {
-            this.message = message;
-            repaint(); // 强制重新绘制组件
-        }
     }
 }
